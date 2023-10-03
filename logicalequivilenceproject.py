c4608b89ecda0c0731ea5163f9805702813c0f39
STellam73 <144480837+STellam73@users.noreply.github.com>
2023-10-03 13:44:21 -0400

    Create logicalequivilenceproject.py

---
+++ logicalequivilenceproject.py
@@ -0,0 +1,20 @@
+#logical equivilence project
+#input 1 is it raining
+#input 2 do you have an umbrella
+#input 3 you went outside today
+x = input("is it raining today?")
+y = input("did you have an umbrella today?")
+z = input("you went outside today.")
+
+if x == True: 
+    print("Yes")
+elif x == False:
+    print("no")
+elif y == x:
+    print("Yes")
+elif y == x:
+    print("no")
+elif z == x:
+    print("no")
+else:
+    print("no")
